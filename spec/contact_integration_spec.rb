9a0cf5c7ba1bd316fc8028d5f928806b11db8fee
David Hale <david.hale7@gmail.com>
2017-09-14 18:07:14 -0700

    add css styling

--- spec/contact_integration_spec.rb
+++ spec/contact_integration_spec.rb
@@ -6,12 +6,10 @@ set(:show_exceptions, false)
 describe('contact addition', {:type => :feature}) do
   it('processes the user entry and returns link to their contact information') do
     visit('/')
-    fill_in('first_name', :with => 'Botty')
-    fill_in('last_name', :with => 'McBot')
-    fill_in('company_name', :with => 'Google')
-    fill_in('job_title', :with => 'Ruby Developer')
-    choose('p')
+    fill_in('first_name', :with => 'Eddie')
+    fill_in('last_name', :with => 'Hazel')
+    fill_in('number', :with => '555-186-2823')
     click_button('Add!')
-    expect(page).to have_content('Botty McBot')
+    expect(page).to have_content('Eddie Hazel')
   end
 end
